058b7cdd4339f500464d8de286911cbd71c4e776
Daniel Moscardini <moscardinibdaniel@gmail.com>
2020-12-03 13:00:56 -0300

    Adicionando Desafios
    Adicionando do exercicio 73 até o exercicio 77.

--- pythonProject/Curso Em Video Python/Mundo 3/ex073.py
+++ pythonProject/Curso Em Video Python/Mundo 3/ex073.py
@@ -1,5 +1,10 @@
 """
 Crie uma tupla preenchida com os 20 primeiros numeros colocados da tabela do campeonato brasileiro de futebol, na
-ordem de c
+ordem de colocação. Depois mostre:
+A) Apenas os 5 primeiros colocados
+B) Os últimos 4 colocados da tabela
+C) Uma lista com os time em ordem alfabética
+D) Em que posição na tabela, esta o time chapecoense
+
 
 """
