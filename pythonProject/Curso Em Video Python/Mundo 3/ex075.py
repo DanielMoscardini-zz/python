6a1aa3729668016888ed1ef49a48f9f639bad5d7
Daniel Moscardini <moscardinibdaniel@gmail.com>
2020-12-03 17:38:51 -0300

    Update ex075.py

--- pythonProject/Curso Em Video Python/Mundo 3/ex075.py
+++ pythonProject/Curso Em Video Python/Mundo 3/ex075.py
@@ -3,5 +3,4 @@ Desenvolva um programa que leia 4 valores pelo teclado e guarde-os em uma tupla.
 A) Quantas vezes apareceu o valor 9
 B) Em que posição foi digitado o primeiro valor 3
 C) Quais foram os numeros pares
-
 """
