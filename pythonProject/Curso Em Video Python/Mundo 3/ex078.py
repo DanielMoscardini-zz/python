8905b00f4f8183a327b2429ef686b3bc11d53387
Daniel Moscardini <moscardinibdaniel@gmail.com>
2020-12-08 14:30:49 -0300

    Descrição dos exercicios de lista
    Aula 16 e exercicios 78 até 83

---
+++ pythonProject/Curso Em Video Python/Mundo 3/ex078.py
@@ -0,0 +1,4 @@
+"""
+Faça um programa que leia 5 valores numericoes e guarde-os em uma lista. No final, mostre qual foi o mairo
+e o menor valor digitado e as suas respectivas posições na lista.
+"""
---
+++ pythonProject/Curso Em Video Python/Mundo 3/ex079.py
@@ -0,0 +1,5 @@
+"""
+Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. Caso o numero
+já exista dentro da lista, ele não será adicionado. No final, serão exibidos todos os valores únicos digitados,
+em ordem crescente.
+"""
